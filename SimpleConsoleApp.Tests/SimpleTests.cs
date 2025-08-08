ea5da3b9d498a2ebfadb4edce631050d1aeabf10
Sanjay <sanjay.bajracharya@aremedia.com.au>
2025-08-08 16:12:59 +1000

    Initial Commit

---
+++ SimpleConsoleApp.Tests/SimpleTests.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleConsoleApp.Tests;
+
+public class SimpleTests
+{
+    [Theory]
+    [InlineData(4,5, 9)]
+    [InlineData(16,4, 20)]
+    public void Maths_AddingTwoNumbers_ShouldReturnCorrectValue(int number1, int number2, int expectedResult)
+    {
+        // Act
+        int result = Maths.Add(number1, number2);
+        
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ SimpleConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(Maths.Add(3, 4));
+
+public static class Maths
+{
+    public static int Add(int number1, int number2)
+    {
+        return number1 + number2;
+    }
+}
